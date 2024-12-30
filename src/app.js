53f38e4f3a9e03e33933a2dd8b7598f4cfc93290
rmsgnl11 <rmsgnl11@naver.com>
2024-12-31 00:51:36 +0900

    가짜 gpt

--- src/app.js
+++ src/app.js
@@ -63,7 +63,7 @@ function scrollToBottom() {
 function getAssistantResponse(userMessage) {
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve("This is a simulated response. You said: " + userMessage);
+      resolve("나는 가짜 gpt야. You said: " + userMessage);
     }, 1500);
   });
 }
